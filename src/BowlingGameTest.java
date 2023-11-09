038a9913c33e70979c66f615e3c9827348ec4530
TrevorEngland23 <trevor.england.523@gmail.com>
2023-11-09 15:00:43 -0800

    refactor tests one and two

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -2,13 +2,22 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class BowlingGameTest {
+    /**
+     * @param game game to repeat rolls on
+     * @param rolls Number of rolls to repeat
+     * @param pins Number of pins per roll to record
+     */
+
+    private static void rollMany(Game game, int rolls,int pins){
+        for(int roll = 0; roll < rolls; roll++){
+            game.roll(pins);
+        }
+    }
+
     @Test
     public void allGuttersGameShouldBeZero(){
         Game game = new Game();
-        for(int i = 0; i < 20; i++){
-            game.roll(0);
-        }
-
+        rollMany(game, 20, 0);
         assertEquals(0, game.score());
     }
 
@@ -16,10 +25,7 @@ public class BowlingGameTest {
 
     public void allOnesGameShouldScoreTwenty() {
         Game game = new Game();
-        for(int i = 0; i < 20; i++){
-            game.roll(1);
-        }
-
+        rollMany(game, 20, 1);
         assertEquals(20, game.score());
     }
-}
+ }
