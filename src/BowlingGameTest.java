bcfe9b9a310ec5efee41c15e181b61662e7d57e9
TrevorEngland23 <trevor.england.523@gmail.com>
2023-11-09 14:40:00 -0800

    Test one complete

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -1,3 +1,15 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class BowlingGameTest {
-    
+    @Test
+    public void allGuttersGameShouldBeZero(){
+        Game game = new Game();
+        for(int i = 0; i < 20; i++){
+            game.roll(0);
+        }
+
+        assertEquals(0, game.score());
+    }
 }
